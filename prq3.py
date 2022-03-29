ce60cb9d1d9e2afa9e3a0346420d1a373d3bf971
unknown <lsjaivenkatesh@outlook.com>
2022-03-29 17:19:49 +0530

    first commit

---
+++ prq3.py
@@ -0,0 +1,5 @@
+age = int(input("Enter age of the person"))
+if age >= 60:
+    print("Person is a senior Citizen")
+else:
+    print("The person is not a senior Citizen")
---
+++ prq4.py
@@ -0,0 +1,9 @@
+first = float(input('Enter first number: '))
+second = float(input('Enter second number: '))
+
+if first < second:
+    small = first
+else:
+    small = second
+
+print('Smallest = %d' %(small))
